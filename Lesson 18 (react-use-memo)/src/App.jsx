3b8e7baef9e3c6a507f83ea7cbd53031c5c3186b
Lameed88 <moradeyor88@gmail.com>
2023-11-09 10:50:39 +0100

    update

--- Lesson 18 (react-use-memo)/src/App.jsx
+++ Lesson 18 (react-use-memo)/src/App.jsx
@@ -32,7 +32,7 @@ const App = () => {
   return(
     <main className="App">
       <label htmlFor="">Fibonacci Sequence</label>
-      <input type="text" placeholder="input" value={userInput} onChange={(e) => setUserInput(e.target.value)}/>
+      <input type="text" placeholder="position" value={userInput} onChange={(e) => setUserInput(e.target.value)}/>
       <h1>output:{userInput || ".."}</h1>
     </main>
   )
