f158b4e0db2e65f354c9a3ea22daf387420917fd
Lameed88 <moradeyor88@gmail.com>
2023-11-09 12:28:50 +0100

    update

--- Lesson 19 (react-timer)/src/App.jsx
+++ Lesson 19 (react-timer)/src/App.jsx
@@ -15,7 +15,9 @@ const App = () => {
   const startTimer = () => 
   timerId.current = setInterval(() => {
     renders.current++;
-  })
+    setSeconds(prev => prev + 1 )
+  }, 1000)
+}
 
 
   const handleInputChange = (e) => {
