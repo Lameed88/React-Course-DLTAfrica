720f4b2a021e176d44363954e0a5dd7a09ac6a9d
Lameed88 <moradeyor88@gmail.com>
2023-11-09 14:44:37 +0100

    update

--- Lesson 20 (use-reducer/src/App.jsx
+++ Lesson 20 (use-reducer/src/App.jsx
@@ -1,47 +1,33 @@
-import { useState, useRef } from "react";
-
+import { useState } from "react";
 
 const App = () => {
+  const [userInput, setUserInput] = useState("");
+  const [count, setCount] = useState(0);
+  const [color, setColor] = useState(false);
 
-  const [userInput, setUserInput] = useState('')
-  const [count, setCount] = useState(0)
-  const [color, setColor] = useState(false)
-  
   return (
-    <main className="App" style={{color: color ? '#fff' : '#fff92'}}>
+    <main className="App" style={{ color: color ? "#fff" : "#fff92" }}>
       <input
-    
-       
+        onChange={(e) => setUserInput(e.target.value)}
         type="text"
-        value={randomInput}
-        onChange={(e) => setUserInput (e.target.value)}
-
-      />
+        value={userInput}
      
+      />
 
       <br />
       <br />
-
-      <p>Count</p>
+      <p> {count}</p>
 
       <section>
-        <button onClick={setCount(prev => prev-1 )}>-</button>
-        <button onClick={setCount(prev => prev+1 )}>+</button>
-        <button onClick={setColor(prev => !prev )}>Color</button>
-       
+        <button onClick={() => setCount((prev) => prev - 1)}>-</button>
+        <button onClick={() => setCount((prev) => prev + 1)}>+</button>
+        <button onClick={() => setColor((prev) => !prev)}>Color</button>
       </section>
 
-      <button onClick={resetTimer}>Reset</button>
-
       <br />
       <br />
 
-      <p>Seconds: {Seconds}</p>
-
-      <br />
-      <br />
-
-      <p>{randomInput}</p>
+      <p>{userInput}</p>
     </main>
   );
 };
