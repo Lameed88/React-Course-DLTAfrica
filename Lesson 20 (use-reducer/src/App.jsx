b8a1ca0e40680976fd4765332d9eb56a2fa740e5
Lameed88 <moradeyor88@gmail.com>
2023-11-09 15:24:45 +0100

    update

--- Lesson 20 (use-reducer/src/App.jsx
+++ Lesson 20 (use-reducer/src/App.jsx
@@ -3,33 +3,37 @@ import { useReducer, useState } from "react";
 const reducer = (state, action) => {
   switch(action.type) {
     case'increment':
-    return{count: state.count + 1}
+    return{...state, count: state.count + 1}
     case'decrement':
-    return{count: state.count - 1}
+    return{..state, count: state.count - 1}
+    case 'newUserInput':
+      return {userInput: action.payload}
+      case'tgColor':
+      return {...state, color: !state.color}
     default:
       throw new Error()
   }
 }
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer,{count: 0})
-  const [userInput, setUserInput] = useState("");
-  // const [count, setCount] = useState(0);
-  const [color, setColor] = useState(false);
+  const [state, dispatch] = useReducer(reducer,{count: 0, userInput:'', color:false})
+  // const [userInput, setUserInput] = useState("");
+  // // const [count, setCount] = useState(0);
+  // const [color, setColor] = useState(false);
 
   return (
-    <main className="App" style={{ color: color ? "#fff" : "#fff952" }}>
+    <main className="App" style={{ color: state.color ? "#fff" : "#fff952" }}>
       <input
      
         type="text"
         value={userInput}
-        onChange={(e) => setUserInput(e.target.value)}
+        onChange={(e) => dispatch({type: 'newUserInput', payload: e.target.value}))}
      
       />
 
       <br />
       <br />
-      <p> {count}</p>
+      <p> {state.count}</p>
 {/* use state */}
       {/* <section>
         <button onClick={() => setCount((prev) => prev - 1)}>-</button>
