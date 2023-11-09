a2bf081a2f648a9a34423f3a1d0cb70448413077
Lameed88 <moradeyor88@gmail.com>
2023-11-09 11:57:47 +0100

    update

--- Lesson 18 (react-use-memo)/src/App.jsx
+++ Lesson 18 (react-use-memo)/src/App.jsx
@@ -15,7 +15,6 @@ const App = () => {
       <input type="text" 
       value={randomInput}
       placeholder='Type anything...'
-      onChange={(e) => setRandomInput(e.target.value)}
       />
       <p>Renders: {randomInput}</p>
 
