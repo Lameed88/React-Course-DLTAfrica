c42c4db1dbf3796b63f3bef5fbe865b7c372956c
Lameed88 <moradeyor88@gmail.com>
2023-11-09 10:13:06 +0100

    update

--- Lesson 18 (react-use-memo)/src/App.jsx
+++ Lesson 18 (react-use-memo)/src/App.jsx
@@ -11,22 +11,7 @@ const getArray = () => {
 
 const App = () => {
   const [userInput, setUserInput] = useState("");
-  const [result, setResult] = useState(0)
-  const [num1] = useState(4);
-  const [num2] = useState(5);
 
-
-  const sum = useCallback(()  => num1 + num2)
-
-  const buildArray = useCallback(() => [num1,num2], [num1,num2])
-
-  
-
-  useEffect(() =>{
-    console.log(`New sum.value: ${sum()}`);
-    setResult(sum())
-
-  },[sum,buildArray])
 
   return(
     <main className="App">
