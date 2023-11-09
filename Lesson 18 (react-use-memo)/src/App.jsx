dc70d560cffd80c71d7c69046897e8e8a7459255
Lameed88 <moradeyor88@gmail.com>
2023-11-09 12:23:47 +0100

    update

--- Lesson 18 (react-use-memo)/src/App.jsx
+++ Lesson 18 (react-use-memo)/src/App.jsx
@@ -1,55 +1,50 @@
-import { useState, useRef } from "react";
+import { useState, useEffect, useCallback, useMemo } from "react";
 
+import React from "react";
 
+const getArray = () => {
+  for(let i = 0; i < 100000000; i++) {
+
+  }
+  return ['Muhammad', "Fine Boy"]
+}
 
 const App = () => {
+  const [userInput, setUserInput] = useState("");
   const [randomInput, setRandomInput] = useState("");
-  const [seconds, setSeconds] = useState("0");
 
-  const renders = useRef(0);
+  const fib = useCallback((n) => {
+    return n <= 1 ? n : fib(n-1) + fib(n-2)  
 
-  const inputRef =useRef()
+  }, [])
 
-  const handleInputChange = (e) => {
-    setRandomInput(e.targetvalue);
 
-    renders.current++
-  }
-  const focusOnInput = () => {
-    inputRef.current.focus()
-  }
+  const fibNumber = useMemo(() => fib(userInput), [userInput, fib])
+
+  const myArray = useMemo(() => getArray(), [])
+  useEffect(() => {
+    console.log("New Array");
+  }, [myArray])
+
 
   return(
     <main className="App">
-  
-      <input 
-      ref={inputRef}
-      type="text" 
+      <label htmlFor="">Fibonacci Sequence</label>
+      <input type="text" placeholder="position" value={userInput} onChange={(e) => setUserInput(e.target.value)}/>
+
+     
+      <p>Number: {fibNumber || "no number added yet"}</p>
+      <br />
+      <br />
+
+
+      <label htmlFor="">Random Input:</label>
+      <input type="text" 
       value={randomInput}
       placeholder='Type anything...'
-      onChange={handleInputChange}
+      onChange={(e) => setRandomInput(e.target.value)}
       />
-      <p>Renders: {renders.current}</p>
-
-      <br />
-      <br />
-      <section>
-        <button onClick={focusOnInput}>Focus</button>
-        <button>Stop</button>
-        <button>Start</button>
-      </section>
-
-      <button>Reset</button>
-
-      <br />
-      <br />
-
-      <p>Seconds:</p>
-
-      <br />
-      <br />
-      <p>{randomInput}</p>
-
+      <p>Result: {randomInput}</p>
     </main>
   )
 
---
+++ Lesson 19 (react-timer)/src/App.jsx
@@ -0,0 +1,59 @@
+import { useState, useRef } from "react";
+
+
+
+const App = () => {
+  const [randomInput, setRandomInput] = useState("");
+  const [seconds, setSeconds] = useState("0");
+
+  const renders = useRef(0);
+
+  const inputRef =useRef()
+
+  const handleInputChange = (e) => {
+    setRandomInput(e.targetvalue);
+
+    renders.current++
+  }
+  const focusOnInput = () => {
+    inputRef.current.focus()
+  }
+
+  return(
+    <main className="App">
+  
+      <input 
+      ref={inputRef}
+      type="text" 
+      value={randomInput}
+      placeholder='Type anything...'
+      onChange={handleInputChange}
+      />
+      <p>Renders: {renders.current}</p>
+
+      <br />
+      <br />
+      <section>
+        <button onClick={focusOnInput}>Focus</button>
+        <button>Stop</button>
+        <button>Start</button>
+      </section>
+
+      <button>Reset</button>
+
+      <br />
+      <br />
+
+      <p>Seconds:</p>
+
+      <br />
+      <br />
+      <p>{randomInput}</p>
+
+    </main>
+  )
+
+}
+
+
+export default App;
