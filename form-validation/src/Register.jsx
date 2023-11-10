d409d994dc6b73ab6df670759ff0e41a62433e2e
Lameed88 <moradeyor88@gmail.com>
2023-11-10 10:11:27 +0100

    update

--- form-validation/src/Register.jsx
+++ form-validation/src/Register.jsx
@@ -3,4 +3,6 @@ import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icon
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "./api/axios"
 
+const USER_REGEX = /^[A-Z][A-Z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/
 const 
