8bb29000bc23ae29e3dadf8aab87aa397531e5ae
Lameed88 <moradeyor88@gmail.com>
2023-11-10 10:45:38 +0100

    update

--- form-validation/src/Register.jsx
+++ form-validation/src/Register.jsx
@@ -1,27 +1,42 @@
 import { useRef, useState, useEffect } from "react";
-import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCheck,
+  faTimes,
+  faInfoCircle,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import axios from "./api/axios"
+import axios from "./api/axios";
 
-const USER_REGEX = /^[A-Z][A-Z0-9-_]{3,23}$/;
-const PWD_REGEX = /^(?=.*[a-z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/
-const REGISTER_URL = "/Register"
-
-
+const USER_REGEX = /^[A-Z][a-z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+const REGISTER_URL = "/Register";
 
 const Register = () => {
-    const userRef = useRef()
-    const errRef = useRef()
+  const userRef = useRef();
+  const errRef = useRef();
 
-    const [user, setUser] = useState('')
-    const [validName, setValidName] = useState(false)
-    const  [pwdFocus, setPwdFocus] = useState(false)
+  const [user, setUser] = useState("");
+  const [validName, setValidName] = useState(false);
+  const [userFocus, setUserFocus] = useState(false);
 
-    const [matchPwd, setMatchPwd] 
+  const [pwd, setPwd] = useState("");
+  const [validPwd, setValidPwd] = useState(false);
+  const [pwdFocus, setPwdFocus] = useState(false);
 
-  return (
-    <div>Register</div>
-  )
-}
+  const
 
-export default Register
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    useRef.current.focus;
+  }, []);
+
+  useEffect(() => {
+    setValidName(USER_REGEX.test(user));
+  }, [test]);
+
+  return <div>Register</div>;
+};
+
+export default Register;
