dc6a7f5173f5d8304d5711d524630bfa784f04e6
Lameed88 <moradeyor88@gmail.com>
2023-11-10 10:17:12 +0100

    update

--- form-validation/src/Register.jsx
+++ form-validation/src/Register.jsx
@@ -5,4 +5,16 @@ import axios from "./api/axios"
 
 const USER_REGEX = /^[A-Z][A-Z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/
-const REGISTER_URL = "./Register.jsx"
+const REGISTER_URL = "/Register"
+
+
+
+const Register = () => {
+    const userRef = useRef()
+    const errRef = useRef()
+  return (
+    <div>Register</div>
+  )
+}
+
+export default Register
