b6a9a6e40b728bb41e42f3d38cbc0838d98aa15e
Lameed88 <moradeyor88@gmail.com>
2023-11-13 11:02:09 +0100

    update

--- Lesson 21 (form-validation)/src/Register.jsx
+++ Lesson 21 (form-validation)/src/Register.jsx
@@ -48,6 +48,7 @@ const Register = () => {
 
   return (
     <>
+    <section>
       <p
         ref={errRef}
         className={errMsg ? "errmsg" : "offscreen"}
@@ -135,10 +136,22 @@ const Register = () => {
 
         <label htmlFor="confirm_pwd">
           Confirm Password:
-          <FontAwesomeIcon icon={faCheck} className={validMatch && matchPwd ? "valid" : "hide" }
-          <FontAwesomeIcon icon={faTimes} className={validMatch && matchPwd ? "hide" : "invalid "}
+          <FontAwesomeIcon icon={faCheck} className={validMatch && matchPwd ? "valid" : "hide" }/>
+          <FontAwesomeIcon icon={faTimes} className={validMatch || !matchPwd ? "hide" : "invalid " }/>
         </label>
+        <input
+        type="password"
+        id="confirm_pwd"
+        onChange={(e) => setMatchPwd(e.target.value)}
+        value={matchPwd}
+        required
+        aria-invalid={validMatch ? "false" : "true"}
+        aria-describedby="confirmnote"
+        onFocus={() => setMatchFocus(true) }
+        onBlur={() => setMatchFocus(false) }
+        />
       </form>
+      </section>
     </>
   );
 };
