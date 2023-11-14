559b0f666efa0f3a39b84e1734cf04631be39705
Lameed88 <moradeyor88@gmail.com>
2023-11-14 14:46:11 +0100

    update

--- Lesson 22 (form-authentication)/src/App.jsx
+++ Lesson 22 (form-authentication)/src/App.jsx
@@ -1,28 +1,21 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Login from './components/Login'
-import Layout from './components/Login'
-import LinkPage from './components/LinkPage'
-import Unauthorized from './components/Unauthorized'
-
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Layout from "./components/Login";
+import LinkPage from "./components/LinkPage";
+import Unauthorized from "./components/Unauthorized";
 
 const App = () => {
   return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* Public routes */}
+        <Route Path="login" element={<Login />} />
+        <Route Path="linkPage" element={<LinkPage />} />
+        <Route Path="unauthorized" element={<Unauthorized />} />
+      </Route>
+    </Routes>
+  );
+};
 
-      <Routes>
-
-        <Route path='/' element={<Layout/>}>
-          {/* Public routes */}
-          <Route Path='login' element={<Login/>}/>
-          <Route Path='linkPage' element={<LinkPage/>}/>
-          <Route Path='unauthorized' element={<Unauthorized/>}/>
-
-
-          </Route>
-        </Routes>
-   
-  )
-}
-
-export default App
+export default App;
