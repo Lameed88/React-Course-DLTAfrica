8cd798377cfc1f801143af7c312f30fca6828923
Lameed88 <moradeyor88@gmail.com>
2023-11-14 10:09:37 +0100

    update

--- Lesson 22 (form-authentication)/src/Login.jsx
+++ Lesson 22 (form-authentication)/src/Login.jsx
@@ -29,11 +29,24 @@ const Login = () => {
             headers: {'Content-Type': 'application/json'},
             withCredentials: true
         })
+        console.log(JSON.stringify(response?.data));
+        const accessToken = response?.data?.accessToken
+        const roles = response?.data?.roles
+        setAuth({user, pwd, roles, accessToken})
       setSuccess(true);
       setUser("");
       setPwd("");
     } catch (err) {
-
+        if (!err?.response) {
+            setErrMsg('No Server Response')
+        }else if (err.response?.status === 400) {
+            setErrMsg('Missing Username or Password')
+        }else if (err.response?.status === 401) {
+            setErrMsg('Unauthorized')
+        }else{
+            setErrMsg('Login Failed')
+        }
+        errRef.current.focus()
     }
     // if(!username || !pwd) {
     //     setErrMsg('Invalid Entry');
