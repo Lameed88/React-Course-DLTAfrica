4a56ee8cc346b2f65de8d16f82160f798438ff5b
Lameed88 <moradeyor88@gmail.com>
2023-11-14 15:55:32 +0100

    update

--- Lesson 22 (form-authentication)/src/components/RequireAuth.jsx
+++ Lesson 22 (form-authentication)/src/components/RequireAuth.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { useLocation, Navigate, Outlet } from 'react-router-dom'
 
-const RequireAuth = () => {
+const RequireAuth = ({allowedRoles}) => {
     const {auth} = useAuth()
     const Location = useLocation()
   return (
+    auth?.roles?.find(role => allowedRoles?.includes(role)) 
+    ?
+    <Outlet />
 
   )
 }
