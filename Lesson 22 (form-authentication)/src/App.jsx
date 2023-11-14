d7a24d8aabea43b27bb250b8b52a8b65dd252444
Lameed88 <moradeyor88@gmail.com>
2023-11-14 14:45:52 +0100

    update

--- Lesson 22 (form-authentication)/src/App.jsx
+++ Lesson 22 (form-authentication)/src/App.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Login from './components/Login'
+import Layout from './components/Login'
+import LinkPage from './components/LinkPage'
+import Unauthorized from './components/Unauthorized'
+
 
 
 const App = () => {
   return (
-    <main className='App'>
-      
-    </main>
+
+      <Routes>
+
+        <Route path='/' element={<Layout/>}>
+          {/* Public routes */}
+          <Route Path='login' element={<Login/>}/>
+          <Route Path='linkPage' element={<LinkPage/>}/>
+          <Route Path='unauthorized' element={<Unauthorized/>}/>
+
+
+          </Route>
+        </Routes>
+   
   )
 }
 
