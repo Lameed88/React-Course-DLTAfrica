ea0009203657f25c6cc45ed8166fc31cc2c4f036
Lameed88 <moradeyor88@gmail.com>
2023-11-14 09:30:16 +0100

    update

--- Lesson 22 (form-authentication)/src/Login.jsx
+++ Lesson 22 (form-authentication)/src/Login.jsx
@@ -3,13 +3,13 @@ import AuthContext from "./context/AuthProvider";
 import axios from "./api/axios";
 
 const Login = () => {
-    const LOGIN_URL = '/auth'
-    const { setAuth } = useContext(AuthContext)
+  const LOGIN_URL = "/auth";
+  const { setAuth } = useContext(AuthContext);
 
   const userRef = useRef();
   const errRef = useRef();
 
-  const [username, setUsername] = useState("");
+  const [user, setUser] = useState("");
   const [password, setPassword] = useState("");
   const [errMsg, setErrMsg] = useState("");
   const [success, setSuccess] = useState(false);
@@ -20,78 +20,77 @@ const Login = () => {
 
   useEffect(() => {
     setErrMsg("");
-  }, [username, password]);
+  }, [user, password]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-        
-    } catch (error) {
-        
+
+    //   const response = await axios.post(LOGIN_URL, JSON.stringify((user, pwd)));
+      setSuccess(true);
+      setUser("");
+      setPassword("");
+    } catch (err) {
+
     }
     // if(!username || !password) {
     //     setErrMsg('Invalid Entry');
     //     return;
     //   }
 
-    setSuccess(true);
-    console.log(username,  password);
-    setUsername()
-    setPassword
-
+    console.log(user, password);
+ 
   };
 
   return (
     <>
-    {success ? (
-      <section>
-        <h1>
-          You have successfully Login
-        </h1>
-        <p>
-          <a href="home">Home </a>
-        </p>
-      </section>
-    ):(
-    <section>
-      <p
-        ref={errRef}
-        className={errMsg ? "errMsg" : "offscreen"}
-        aria-live="assertive"
-      >
-        {errMsg}
-      </p>
-      <h1>Sign In</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          ref={userRef}
-          autoComplete="off"
-          onChange={(e) => setUsername(e.target.value)}
-          value={username}
-          required
-        />
-        <label htmlFor="password">
-          Password
-          <input
-            type="password"
-            id="password"
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            required
-          />
-        </label>
+      {success ? (
+        <section>
+          <h1>You have successfully Login</h1>
+          <p>
+            <a href="home">Home </a>
+          </p>
+        </section>
+      ) : (
+        <section>
+          <p
+            ref={errRef}
+            className={errMsg ? "errMsg" : "offscreen"}
+            aria-live="assertive"
+          >
+            {errMsg}
+          </p>
+          <h1>Sign In</h1>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="user">User</label>
+            <input
+              type="text"
+              id="username"
+              ref={userRef}
+              autoComplete="off"
+              onChange={(e) => setUser(e.target.value)}
+              value={user}
+              required
+            />
+            <label htmlFor="password">
+              Password
+              <input
+                type="password"
+                id="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                required
+              />
+            </label>
 
-        <button>Sign Up</button>
-      </form>
-      <p>
-        Need an Account? <br />
-      </p>
-    </section>
-    )}
+            <button>Sign Up</button>
+          </form>
+          <p>
+            Need an Account? <br />
+          </p>
+        </section>
+      )}
     </>
   );
 };
