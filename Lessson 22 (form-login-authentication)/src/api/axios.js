db94aebf27e7e382a089f4931104047eb01e3750
Lameed88 <moradeyor88@gmail.com>
2023-11-15 15:21:01 +0100

    update

--- Lessson 22 (form-login-authentication)/src/api/axios.js
+++ Lessson 22 (form-login-authentication)/src/api/axios.js
@@ -1,5 +1,13 @@
 import axios from "axios";
 
+const BASE_URL = 'http://localhost:3500'
+
 export default axios.create({
-    baseURL: "http://localhost:3000"
-})
+    baseURL: BASE_URL,
+}) 
+
+export const axiosPrvate = axios.create({
+    baseURL: BASE_URL,
+    headers: {'Content-Type':'apllication/json'},
+    withCredentials: true
+}) 
--- Lessson 22 (form-login-authentication)/src/components/Users.jsx
+++ Lessson 22 (form-login-authentication)/src/components/Users.jsx
@@ -1,33 +1,33 @@
 import { useState, useEffect } from "react";
 import axios from "../api/axios";
 
+
+
 const Users = () => {
-  const [users, setUsers] = useState();
+  const [users, setUsers] = useState([]);
+
 
   useEffect(() => {
     let isMounted = true;
-    const controller = new AbortController()
+    const controller = new AbortController();
 
     const getUsers = async () => {
-        try {
-            const response = await axios.get('/users', {
-
-            })
-            console.log(response.data);
-            isMounted && setUsers(response.data)
-        } catch (err) {
-            console.error(err);
-        }
-    }
-     getUsers()
-     return() => {
-        isMounted = false
-        controller.abort()
-
-    }
-
-
-  }, [])
+      try {
+        const response = await axios.get("/users", {
+          signal: controller.signal,
+        });
+        console.log(response.data);
+        isMounted && setUsers(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getUsers();
+    return () => {
+      isMounted = false;
+      controller.abort();
+    };
+  }, []);
 
   return (
     <article>
@@ -35,12 +35,14 @@ const Users = () => {
       {users?.length ? (
         <ul>
           {users.map((user, i) => (
-            <li key={i}> {user?.username}</li>
+            <li key={i}>{user?.username}</li>
           ))}
         </ul>
       ) : (
         <p>No user to display</p>
       )}
+     
+      <br />
     </article>
   );
 };
