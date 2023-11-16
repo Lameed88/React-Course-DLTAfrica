9ba59b91d2eb69467a155e7d6dcf1bde6ef1e5e6
Lameed88 <moradeyor88@gmail.com>
2023-11-16 11:53:51 +0100

    update

--- Lessson 22 (form-login-authentication)/src/components/PersistLogin.jsx
+++ Lessson 22 (form-login-authentication)/src/components/PersistLogin.jsx
@@ -27,7 +27,9 @@ const PersistLogin = () => {
         return() => isMounted = false
     }, [])
 
-    useEffect(() => )
+    useEffect(() => {
+        console.log(`isLoading: ${loaing}`);
+    })
 
 }
 
