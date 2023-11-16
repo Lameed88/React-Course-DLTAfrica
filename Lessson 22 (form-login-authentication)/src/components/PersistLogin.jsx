20c1d7ed4842fee1271f46fc7a15e1b591d6bead
Lameed88 <moradeyor88@gmail.com>
2023-11-16 15:03:42 +0100

    update

--- Lessson 22 (form-login-authentication)/src/components/PersistLogin.jsx
+++ Lessson 22 (form-login-authentication)/src/components/PersistLogin.jsx
@@ -1,48 +1,37 @@
-import { Outlet } from "react-router-dom"
-import { useState, useEffect } from "react"
-import useRefreshToken from "../hooks/useRefreshToken "
-import useAuth from "../hooks/useAuth"
+import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
+import useRefreshToken from "../hooks/useRefreshToken ";
+import useAuth from "../hooks/useAuth";
 
 const PersistLogin = () => {
-    const [isLoading, setIsLoading] = useState(true)
-    const refresh = useRefreshToken()
+  const [isLoading, setIsLoading] = useState(true);
+  const refresh = useRefreshToken();
 
-    const { auth } = useAuth();
+  const { auth, persist } = useAuth();
 
-    useEffect(() => {
+  useEffect(() => {
     // let isMounted = true
 
-        const verifyRefreshToken = async () => {
-            try {
-                await refresh()
-            } catch (error) {
-                console.log(error);
-                
-            }finally{
-                setIsLoading(false)
-            }
-        }
-        !auth?.accessToken ? verifyRefreshToken() : setIsLoading(false)
+    const verifyRefreshToken = async () => {
+      try {
+        await refresh();
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    !auth?.accessToken ? verifyRefreshToken() : setIsLoading(false);
 
-        // return() => isMounted = false
-    }, [])
+    // return() => isMounted = false
+  }, []);
 
-    useEffect(() => {
-        console.log(`isLoading: ${isLoading}`);
-        console.log(`aT: ${JSON.stringify(auth?.accessToken)}`);
-    }, [isLoading])
+  useEffect(() => {
+    console.log(`isLoading: ${isLoading}`);
+    console.log(`aT: ${JSON.stringify(auth?.accessToken)}`);
+  }, [isLoading]);
 
-    return (
-        <> 
-        {isLoading ? 
-        <p>Loading</p>
-        : 
-        <Outlet />
-        }
-        
-        </>
-    )
+  return <>{persist ? <Outlet /> : isLoading ? <p>Loading</p> : <Outlet />}</>;
+};
 
-}
-
-export default PersistLogin
+export default PersistLogin;
