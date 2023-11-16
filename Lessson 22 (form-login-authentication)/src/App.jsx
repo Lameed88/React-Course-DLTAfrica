aff7c4f4817dc6f82626467a55fe37df08e9943d
Lameed88 <moradeyor88@gmail.com>
2023-11-16 12:05:37 +0100

    update

--- Lessson 22 (form-login-authentication)/src/App.jsx
+++ Lessson 22 (form-login-authentication)/src/App.jsx
@@ -10,37 +10,44 @@ import Admin from "./components/Admin";
 import Lounge from "./components/Lounge";
 import Missing from "./components/Missing";
 import RequireAuth from "./components/RequireAuth";
+import PersistLogin from "./components/PersistLogin";
 
 const ROLES = {
-  'User' : 2001,
-  'Editor' : 1984,
-  'Admin' : 5150
-}
+  User: 2001,
+  Editor: 1984,
+  Admin: 5150,
+};
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         {/* public Routes */}
-
         <Route path="login" element={<Login />} />
         <Route path="/linkpage" element={<LinkPage />} />
         <Route path="/Unauthorized" element={<Unauthorized />} />
-
         {/* private Routes */}
-        <Route element={<RequireAuth allowedRoles={[ROLES.User, ROLES.Editor, ROLES.Admin]}/>}>
+        >Route element={PersistLogin}
+        <Route
+          element={
+            <RequireAuth
+              allowedRoles={[ROLES.User, ROLES.Editor, ROLES.Admin]}
+            />
+          }
+        >
           <Route path="/" element={<Home />} />
         </Route>
-        <Route element={<RequireAuth allowedRoles={[ROLES.Editor]}/>}>
+        <Route element={<RequireAuth allowedRoles={[ROLES.Editor]} />}>
           <Route path="editor" element={<Editor />} />
         </Route>
-        <Route element={<RequireAuth allowedRoles={[ROLES.Admin]}/>}>
+        <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
           <Route path="admin" element={<Admin />} />
         </Route>
-        <Route element={<RequireAuth allowedRoles={[ROLES.Editor, ROLES.Admin]}/>}>
+        <Route
+          element={<RequireAuth allowedRoles={[ROLES.Editor, ROLES.Admin]} />}
+        >
           <Route path="lounge" element={<Lounge />} />
         </Route>
-        
         {/* to catch all error */}
         <Route path="*" element={<Missing />} />
       </Route>
