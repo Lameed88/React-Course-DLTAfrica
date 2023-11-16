af5979f05cc860e0949496793aae30440ea5161f
Lameed88 <moradeyor88@gmail.com>
2023-11-16 11:56:17 +0100

    update

--- Lessson 22 (form-login-authentication)/src/components/PersistLogin.jsx
+++ Lessson 22 (form-login-authentication)/src/components/PersistLogin.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, json } from "react-router-dom"
 import { useState, useEffect } from "react"
 import useRefreshToken from "../hooks/useRefreshToken "
 import useAuth from "../hooks/useAuth"
@@ -29,8 +29,8 @@ const PersistLogin = () => {
 
     useEffect(() => {
         console.log(`isLoading: ${loaing}`);
-        console.log();
-    })
+        console.log(`aT: ${json.stringify(auth?. accessToken)}`);
+    }, [isLoading])
 
 }
 
