32d4a856af04d559fdb68667e6ca5783384231e1
Lameed88 <moradeyor88@gmail.com>
2023-11-16 12:40:35 +0100

    update

--- Lessson 22 (form-login-authentication)/src/hooks/useLogout.js
+++ Lessson 22 (form-login-authentication)/src/hooks/useLogout.js
@@ -1,19 +1,20 @@
-import axios from "../api/axios"
-import useAuth from "./useAuth"
+import axios from "../api/axios";
+import useAuth from "./useAuth";
 
 const useLogout = () => {
-    const { setAuth } = useAuth()
-    const logout = async () => {
-        setAuth({})
-        try{
-            const response = await axios('/logout', {
-                withCredentials
-            })
-        }catch(error) {
-            console.log(error);
-        }
+  const { setAuth } = useAuth();
+  const logout = async () => {
+    setAuth({});
+    try {
+      const response = await axios("/logout", {
+        withCredentials,
+      });
+    } catch (error) {
+      console.log(error);
     }
-  return logout
-}
+  };
+  return logout;
+};
 
-export default useLogout
+export default useLogout;
+
