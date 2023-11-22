6cb4a32d0a961704e52e20ccf6192fdc88ff3c70
Lameed88 <moradeyor88@gmail.com>
2023-11-22 10:00:18 +0100

    update

--- infinite-scroll/src/Example1.js
+++ infinite-scroll/src/Example1.js
@@ -2,6 +2,12 @@
 const Example1 = () => {
   return (
     <>
+    <h1 id="top">
+        &infin; Infinite Query &amp; Scroll
+        <br/>
+        &infin; Ex. 1 - React Only
+    </h1>
+
     
     </>
   )
