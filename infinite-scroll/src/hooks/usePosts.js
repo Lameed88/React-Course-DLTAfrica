d03602b13f87ad720c1eba458b7db04a92812b52
Lameed88 <moradeyor88@gmail.com>
2023-11-22 10:25:11 +0100

    update

--- infinite-scroll/src/hooks/usePosts.js
+++ infinite-scroll/src/hooks/usePosts.js
@@ -1,2 +1,14 @@
 import { useState, useEffect } from "react";
 import { getPostsPage } from "../api/axios"
+
+
+const usePosts = () => {
+    const [results, setResults] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(false)
+  return (
+    <div>usePosts</div>
+  )
+}
+
+export default usePosts
