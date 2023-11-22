6b25a89427acf41b8cde61780d6d82248489af1f
Lameed88 <moradeyor88@gmail.com>
2023-11-22 10:15:38 +0100

    update

--- infinite-scroll/src/api/axios.js
+++ infinite-scroll/src/api/axios.js
@@ -4,4 +4,4 @@ export const api = axios.create({
     baseURL: 'https://jsonplaceholder.typicode.com/'
 })
 
-export const getPostsPage = async(pageParam = 1, Option)
+export const getPostsPage = async(pageParam = 1, option)
