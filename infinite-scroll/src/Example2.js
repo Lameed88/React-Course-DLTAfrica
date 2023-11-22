52924e0565ec1243f7f64700d76eb663ae6d10ad
Lameed88 <moradeyor88@gmail.com>
2023-11-22 14:24:55 +0100

    update

---
+++ infinite-scroll/src/Example2.js
@@ -0,0 +1,77 @@
+import { useRef, useCallback } from "react";
+import { confirmAlert } from "react-confirm-alert";
+import "react-confirm-alert/src/react-confirm-alert.css";
+import Post from "./Post"
+import { useInfiniteQuery } from "react-query";
+import { getPostsPage } from "./api/axios";
+
+const Example2 = () => {
+  const intObserver = useRef()
+
+
+
+
+  const lastPostRef = useCallback ((post) => {
+    if (loading) return
+
+    if (intObserver.current) intObserver.current.disconnect()
+
+    const confirmload = () => {
+      confirmAlert({
+        title: "confirm Load More",
+      message: 'Are you sure to Load more posts',
+      buttons:[
+        {
+          label: 'yes',
+          onClick: () => {
+      setPageNum((prev) => prev + 1)
+      }
+      },
+      {label: 'No'}
+    ]
+   })
+    }
+
+    intObserver.current = new IntersectionObserver((post) => {
+      if (post [0]. isIntersecting && hasNextPage) {
+      console.log("we are near the last post");
+      confirmload()
+    // setPageNum(prev => prev + 1 )
+}
+    }) 
+
+    if (post) intObserver.current.observe(post)
+  },
+[loading, hasNextPage]
+)
+if (isError) return <p className="center">Error : {error.message}</p>;
+
+  const content = results.map((post, i) =>{
+    if (results.length === i + 1) {
+
+    return <Post ref= {lastPostRef} key={post.id} post= {post} />
+
+    }
+    return <Post key={post.id} post= {post} />
+  })
+
+  return (
+    <>
+      <h1 id="top">
+        &infin; Infinite Query &amp; Scroll
+        <br />
+        &infin; Ex. 1 - React Only
+      </h1>
+
+      {content}
+      
+      <p className="center">Loading more posts...</p>
+
+      <p className="center">
+        <a href="#top">Back to Top</a>
+      </p>
+    </>
+  );
+};
+
+export default Example2;
