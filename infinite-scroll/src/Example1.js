cfbf62c8894db8dbcdf3e64b83fa4dd41d543343
Lameed88 <moradeyor88@gmail.com>
2023-11-22 10:03:29 +0100

    update

--- infinite-scroll/src/Example1.js
+++ infinite-scroll/src/Example1.js
@@ -1,24 +1,19 @@
-
 const Example1 = () => {
   return (
     <>
-    <h1 id="top">
+      <h1 id="top">
         &infin; Infinite Query &amp; Scroll
-        <br/>
+        <br />
         &infin; Ex. 1 - React Only
-    </h1>
+      </h1>
 
-    <p className="center">
-        Loading more posts...
-    </p>
+      <p className="center">Loading more posts...</p>
 
-    <p className="center">
+      <p className="center">
         <a href="#top">Back to Top</a>
-
-    </p>
-
+      </p>
     </>
-  )
-}
+  );
+};
 
-export default Example1
+export default Example1;
